bc76dd4135425d421221a7445fe81eba9c58e35f
Raphael Baumann <wayoffproduction@gmail.com>
2017-08-23 21:03:18 +0200

    no change...

--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ import SocketIO from '@/components/SocketIO'
 import VueAnalytics from 'vue-analytics'
 import * as VueGoogleMaps from 'vue2-google-maps'
 
-// const socketInstance = io('http://localhost:3010')
-const socketInstance = io('https://ns3056388.ip-193-70-6.eu', {secure: true})
+const socketInstance = io('http://localhost:3010')
+// const socketInstance = io('https://ns3056388.ip-193-70-6.eu', {secure: true})
 // const socketInstance = io('http://ng2.comem.ch')
 // const socketInstance = io('http://10.192.115.223:3011')
 
