41e2db3da5170a28c83697ec35b97b47596f5e0d
Ryzen <raphael@wayoff.ch>
2017-08-25 10:51:37 +0200

    updated download button size

--- src/components/Ar.vue
+++ src/components/Ar.vue
@@ -5,7 +5,7 @@
       <p>Pour jouer à ce jeu vous devez télécharger une application qui exploite la camera de votre téléphone.</p>
       <p>Téléchargez l'application <a href="#">iOS</a> où <a href="#">Android</a> pour jouer.</p>
       <div class="app-container">
-        <a href="https://itunes.apple.com/ch/app/amplified-reality/id1272141234?mt=8" style="display:inline-block;overflow:hidden;background:url(//linkmaker.itunes.apple.com/assets/shared/badges/en-us/appstore-lrg.svg) no-repeat;width:135px;height:40px;background-size:contain;"></a>
+        <a class="ios" href="https://itunes.apple.com/ch/app/amplified-reality/id1272141234?mt=8" style="display:inline-block;overflow:hidden;background:url(//linkmaker.itunes.apple.com/assets/shared/badges/en-us/appstore-lrg.svg) no-repeat;width:135px;height:40px;background-size:contain;"></a>
         <a class="android" href='https://play.google.com/store/apps/details?id=ch.heigvd.po.amplifiedreality&utm_source=ng17-remote&utm_campaign=ar&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
       </div>
       <!-- <a :href="googleplay-link"><img alt='Disponible sur Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/fr_badge_web_generic.png'/></a> -->
@@ -32,10 +32,15 @@ export default {
   margin: 0 5px;
 }
 .ar .app-container {
+  float: right;
   display: block;
 }
+.ar .ios {
+  float: left;
+  margin-top: 10px;
+}
 .ar a.android img{
-  width: 135px;
+  width: 157px;
   margin: 0;
 }
 </style>
